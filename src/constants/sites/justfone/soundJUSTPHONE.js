2c42c70b98503ac59f72b99fac08b3d37537628e
micheofire <micheofire7@gmail.com>
2024-06-23 16:31:44 +0000

    Added scrapped data

--- src/constants/sites/justfone/soundJUSTPHONE.js
+++ src/constants/sites/justfone/soundJUSTPHONE.js
@@ -1,1 +1,178 @@
-export const soundTable = [];
+export const soundTable = [
+  {
+    "id": 1,
+    "Pname": "Apple AIRPOD 2 CHARGING CASE",
+    "Link": "https://www.justfones.ng/catalogsearch/result/?q=Apple+Airpods+2+WIth+Charging+Case",
+    "H1": "0",
+    "H2": "0",
+    "H3": "0",
+    "L1": "0",
+    "L2": "0",
+    "L3": "0"
+  },
+  {
+    "id": 2,
+    "Pname": "Apple AIRPOD PRO 1ST GEN",
+    "Link": "https://www.justfones.ng/catalogsearch/result/?q=Apple+AIRPOD+PRO+1ST+GEN",
+    "H1": "0",
+    "H2": "0",
+    "H3": "0",
+    "L1": "0",
+    "L2": "0",
+    "L3": "0"
+  },
+  {
+    "id": 3,
+    "Pname": "Apple AIRPODS 3 2021",
+    "Link": "https://www.justfones.ng/catalogsearch/result/?q=Apple+AIRPODS+3+2021",
+    "H1": "0",
+    "H2": "0",
+    "H3": "0",
+    "L1": "0",
+    "L2": "0",
+    "L3": "0"
+  },
+  {
+    "id": 4,
+    "Pname": "Apple AIRPODS PRO 2ND GEN",
+    "Link": "https://www.justfones.ng/catalogsearch/result/?q=Apple+Airpods+Pro+(+2nd+Generation+)",
+    "H1": "0",
+    "H2": "0",
+    "H3": "0",
+    "L1": "0",
+    "L2": "0",
+    "L3": "0"
+  },
+  {
+    "id": 5,
+    "Pname": "SAMSUNG BUDS 2",
+    "Link": "https://www.justfones.ng/catalogsearch/result/?q=SAMSUNG+galaxy+BUDS2,",
+    "H1": "0",
+    "H2": "0",
+    "H3": "0",
+    "L1": "0",
+    "L2": "0",
+    "L3": "0"
+  },
+  {
+    "id": 6,
+    "Pname": "SAMSUNG BUDS 2 PRO",
+    "Link": "https://www.justfones.ng/catalogsearch/result/?q=SAMSUNG+galaxy+BUDS+2+PRO",
+    "H1": "0",
+    "H2": "0",
+    "H3": "0",
+    "L1": "0",
+    "L2": "0",
+    "L3": "0"
+  },
+  {
+    "id": 7,
+    "Pname": "SAMSUNG EARBUDS",
+    "Link": "https://www.justfones.ng/catalogsearch/result/?q=SAMSUNG+galaxy+BUDS",
+    "H1": "120,000",
+    "H2": "0",
+    "H3": "0",
+    "L1": "120,000",
+    "L2": "0",
+    "L3": "0"
+  },
+  {
+    "id": 8,
+    "Pname": "SAMSUNG EARBUDS LIVE",
+    "Link": "https://www.justfones.ng/catalogsearch/result/?q=SAMSUNG+galaxy+BUDS+LIVE",
+    "H1": "120,000",
+    "H2": "0",
+    "H3": "0",
+    "L1": "120,000",
+    "L2": "0",
+    "L3": "0"
+  },
+  {
+    "id": 9,
+    "Pname": "SAMSUNG EARBUDS PLUS",
+    "Link": "https://www.justfones.ng/catalogsearch/result/?q=SAMSUNG+galaxy+EARBUDS+PLUS",
+    "H1": "0",
+    "H2": "0",
+    "H3": "0",
+    "L1": "0",
+    "L2": "0",
+    "L3": "0"
+  },
+  {
+    "id": 10,
+    "Pname": "SAMSUNG EARBUDS PRO",
+    "Link": "https://www.justfones.ng/catalogsearch/result/?q=SAMSUNG+galaxy+EARBUDS+PRO",
+    "H1": "0",
+    "H2": "0",
+    "H3": "0",
+    "L1": "0",
+    "L2": "0",
+    "L3": "0"
+  },
+  {
+    "id": 11,
+    "Pname": "SAMSUNG EARBUDS pro",
+    "Link": "https://www.justfones.ng/catalogsearch/result/?q=SAMSUNG+galaxy+BUDS+LIVE",
+    "H1": "120,000",
+    "H2": "0",
+    "H3": "0",
+    "L1": "120,000",
+    "L2": "0",
+    "L3": "0"
+  },
+  {
+    "id": 12,
+    "Pname": "Samsung Galaxy Buds Live \u2013 Mystic Black",
+    "Link": "https://www.justfones.ng/catalogsearch/result/?q=Samsung+Galaxy+Buds2,",
+    "H1": "0",
+    "H2": "0",
+    "H3": "0",
+    "L1": "0",
+    "L2": "0",
+    "L3": "0"
+  },
+  {
+    "id": 13,
+    "Pname": "Samsung Galaxy Buds2",
+    "Link": "https://www.justfones.ng/catalogsearch/result/?q=Samsung+Galaxy+Type+C+Wired+Earphones",
+    "H1": "0",
+    "H2": "0",
+    "H3": "0",
+    "L1": "0",
+    "L2": "0",
+    "L3": "0"
+  },
+  {
+    "id": 14,
+    "Pname": "Samsung usb c akg earphones",
+    "Link": "https://www.justfones.ng/catalogsearch/result/?q=Beats+By+Dre+Beats+Fit+Pro",
+    "H1": "0",
+    "H2": "0",
+    "H3": "0",
+    "L1": "0",
+    "L2": "0",
+    "L3": "0"
+  },
+  {
+    "id": 15,
+    "Pname": "BEATS FIT PRO",
+    "Link": "https://www.justfones.ng/catalogsearch/result/?q=Beats+By+Dre+Beats+Studio+Buds",
+    "H1": "0",
+    "H2": "0",
+    "H3": "0",
+    "L1": "0",
+    "L2": "0",
+    "L3": "0"
+  },
+  {
+    "id": 16,
+    "Pname": "BEATS STUDIO BUDS",
+    "Link": "https://www.justfones.ng/catalogsearch/result/?q=Beats+By+Dre+Beats+Powerbeats+Pro+Wireless+Earbud",
+    "H1": "0",
+    "H2": "0",
+    "H3": "0",
+    "L1": "0",
+    "L2": "0",
+    "L3": "0"
+  }
+]
