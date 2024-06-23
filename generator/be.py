35288235b2cd01e4eb95262ea784c224a0b9eab7
Micheofire <micheofire7@gmail.com>
2024-06-23 16:33:28 +0100

    updated cors

--- generator/be.py
+++ generator/be.py
@@ -19,7 +19,7 @@ client = OpenAI(api_key=api_key)
 
 app = Flask(__name__)
 
-CORS(app, resources={r"/process_text": {"origins": "*"}})
+CORS(app, resources={r"/process_text": {"origins": "0.0.0.0:5000"}})
 
 @app.route("/process_text", methods=["POST"])
 @cross_origin()
