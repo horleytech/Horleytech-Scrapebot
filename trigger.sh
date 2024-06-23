f41db528e9fa29416f5ca684c75f61db0e08c1aa
Micheofire <micheofire7@gmail.com>
2024-06-23 15:51:09 +0100

    added trigger.sh

--- trigger.sh
+++ trigger.sh
@@ -1,30 +1,31 @@
 git pull
-python3 iphone_justphone.py     
-python3 laptop_slot.py          
-python3 samsung_jiji.py         
-python3 samsung_slot.py         
-python3 smartwatch_slot.py      
-python3 sound_slot.py           
-python3 tablet_slot.py
-python3 iphone_ob.py            
-python3 laptops_jiji.py         
-python3 samsung_ju.py           
-python3 smartwatch_jiji.py      
-python3 sound_jiji.py           
-python3 tablet_jiji.py
-python3 iphone_jiji.py          
-python3 iphone_slot.py          
-python3 laptops_ju.py           
-python3 samsung_justphone.py    
-python3 smartwatch_ju.py        
-python3 sound_ju.py             
-python3 tablet_ju.py
-python3 iphone_ju.py            
-python3 laptop_justphone.py          
-python3 samsung_ob.py           
-python3 smartwatch_justphone.py 
-python3 sound_justphone.py      
-python3 tablet_justphone.py
+source generator/myenv/bin/activate
+python3 generator/iphone_justphone.py     
+# python3 generator/laptop_slot.py          
+# python3 generator/samsung_jiji.py         
+# python3 generator/samsung_slot.py         
+# python3 generator/smartwatch_slot.py      
+# python3 generator/sound_slot.py           
+# python3 generator/tablet_slot.py
+# python3 generator/iphone_ob.py            
+# python3 generator/laptops_jiji.py         
+# python3 generator/samsung_ju.py           
+# python3 generator/smartwatch_jiji.py      
+# python3 generator/sound_jiji.py           
+# python3 generator/tablet_jiji.py
+# python3 generator/iphone_jiji.py          
+# python3 generator/iphone_slot.py          
+# python3 generator/laptops_ju.py           
+# python3 generator/samsung_justphone.py    
+# python3 generator/smartwatch_ju.py        
+# python3 generator/sound_ju.py             
+# python3 generator/tablet_ju.py
+# python3 generator/iphone_ju.py            
+# python3 generator/laptop_justphone.py          
+# python3 generator/samsung_ob.py           
+# python3 generator/smartwatch_justphone.py 
+# python3 generator/sound_justphone.py      
+# python3 generator/tablet_justphone.py
 git add .
 git commit -m "Added scrapped data"
 git push 
