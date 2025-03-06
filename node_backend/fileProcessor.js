ff78f606f990e79faaa0a3fa476faaa3e99d211d
horleytech <156014830+horleytech@users.noreply.github.com>
2025-03-06 12:47:57 +0100

    Update fileProcessor.js

--- node_backend/fileProcessor.js
+++ node_backend/fileProcessor.js
@@ -2,17 +2,28 @@ import fs from 'fs';
 import EventEmitter from 'events';
 import dotenv from 'dotenv';
 import NodeCache from 'node-cache';
-
 import OpenAI from 'openai';
 import nodemailer from 'nodemailer';
 import admin from 'firebase-admin';
 import { groupAndSortPhones } from './dataProcessor.js';
 import { convertString } from './cleaner.js';
 
-dotenv.config();
+// Load .env from one level up (assuming .env is in the root folder of Horleytech-Scrapebot)
+dotenv.config({ path: '../.env' });
+
+// Optional: Debug logging to ensure environment variables are loaded
+console.log("OPENAI_API_KEY:", process.env.OPENAI_API_KEY);
+console.log("FIREBASE_CONFIG:", process.env.FIREBASE_CONFIG);
+console.log("GMAIL_USER:", process.env.GMAIL_USER);
+console.log("GMAIL_PASS:", process.env.GMAIL_PASS);
+console.log("MAILER_FROM_OPTION:", process.env.MAILER_FROM_OPTION);
 
 // ===== START: Firebase Initialization =====
 import { initializeApp, cert } from 'firebase-admin/app';
+if (!process.env.FIREBASE_CONFIG) {
+  console.error("FIREBASE_CONFIG is not set in the environment.");
+  process.exit(1);
+}
 const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG);
 initializeApp({
   credential: cert(firebaseConfig),
@@ -64,53 +75,47 @@ event.on('process', async (data, filePath, title) => {
 
   let finalReponseArray = [];
 
-  let count = 0;
-
   for (const chunk of chunks) {
     const content = `
-    		    Extract Model, Storage (GB), Lock Status, SIM Type, Device Type(iphone, samsung, laptop, watch, sound, tablet) and Price from the text below and return the value as a list of json object with each object having the following keys 'model','storage','lock_status','sim_type’,’device_type’,’price'. 
-                    ${chunk}
-                    Perform the following transformation.
-                    1. If a line contains more than one price specification, extract each price as different json object.
-                    2. Ensure that data is well represented under each key. Ensure that price is in numbers (e.g. 20k should be represented as 20,000). 
-                    3. Remove any record that doesnt have value for all the keys, but if sim_type does not exist, make it Null and make lock_status that does not exist with FU. Fully unlocked should be replaced with FU 
-                    4. Make sure  all product model carries the brand name. e.g. 13 pro max is not a valid model, but iPhone 13 pro max is valid
-                    5. Look out for the condition of the product and add it to the product name. For example, if the product is specified as BRAND NEW, add BRAND NEW to the product name. e.g. BRAND NEW iPhone 15 pro max. Conditions should either be BRAND NEW OR USED. Note that NEW OPENBOX, USED, UK USED, BRAND NEW NO BOX should all fall under USED while BRAND NEW,  NEW SEALED are BRAND NEW.                     
-                    6. If condition specifed is out of the list specifed above, then group them under BRAND NEW or USED using your discretion
-                    7. If condition is not specifed, it is a USED product or if the description says "brand new <model name> only" its also used
-                    8. Ensure that iphones are represented as iPhone, samsung are represente as Samsung, basically make sure that all product name and models are uniform
-                    9. Always return a valid json object, no extra markdown character. Don't add these characters "\`\`\`json".
-                    10. Ensure to always stick to the pattern without any deviation. Your response should not be in markdown. Send it to me as a direct string. Ensure to pass the right data to the right object key. Any value that has the word "unlocked" or “FU”,  must be in the lock_status key. Any value that contains the word "sim" must be in the sim_type key.
-                    11. iPads should be be under tablet device type
-                    12. Ensure that only iphones are under iphone type, only samsung phones are under samsung, only laptops (including macbooks) are under laptop, only watches are under watch, same for sound and tablet
-                    13. For all model names, ensure that you consitently use the same name even if they look different from the provided data 
-                    14. For storage size, always specify the storage unit. e.g. 256GB, 1TB, 128GB.  
-                    15. For laptop and tablets, add all the product specifications available for a product to Lock Status                    
-                    16. For macbooks, always ensure that model year is included under model name. e.g. Used Macbook Pro 2020
-                    17. For tablets and laptops, always ensure that all the availabe specifications are highlighted under lock status
-		    18. Ensure that condition (BRAND NEW or USED) is ALWAYS added to the product name e.g. BRAND NEW iPhone 15 pro max, USED iPhone Xr
+      Extract Model, Storage (GB), Lock Status, SIM Type, Device Type(iphone, samsung, laptop, watch, sound, tablet) and Price from the text below and return the value as a list of json object with each object having the following keys 'model','storage','lock_status','sim_type','device_type','price'. 
+      ${chunk}
+      Perform the following transformation.
+      1. If a line contains more than one price specification, extract each price as different json object.
+      2. Ensure that data is well represented under each key. Ensure that price is in numbers (e.g. 20k should be represented as 20,000). 
+      3. Remove any record that doesn't have value for all the keys, but if sim_type does not exist, make it Null and make lock_status that does not exist with FU. Fully unlocked should be replaced with FU.
+      4. Make sure all product models carry the brand name. e.g. 13 pro max is not a valid model, but iPhone 13 pro max is valid.
+      5. Look out for the condition of the product and add it to the product name. For example, if the product is specified as BRAND NEW, add BRAND NEW to the product name. e.g. BRAND NEW iPhone 15 pro max. Conditions should either be BRAND NEW OR USED. Note that NEW OPENBOX, USED, UK USED, BRAND NEW NO BOX should all fall under USED while BRAND NEW, NEW SEALED are BRAND NEW.                     
+      6. If condition specified is out of the list specified above, then group them under BRAND NEW or USED using your discretion.
+      7. If condition is not specified, it is a USED product or if the description says "brand new <model name> only" it's also used.
+      8. Ensure that iPhones are represented as iPhone, Samsung as Samsung—basically make sure that all product names and models are uniform.
+      9. Always return a valid json object, no extra markdown characters. Don't add these characters "\`\`\`json".
+      10. Ensure to always stick to the pattern without any deviation. Your response should not be in markdown. Send it to me as a direct string. Ensure to pass the right data to the right object key. Any value that has the word "unlocked" or "FU" must be in the lock_status key. Any value that contains the word "sim" must be in the sim_type key.
+      11. iPads should be under tablet device type.
+      12. Ensure that only iPhones are under iphone type, only Samsung phones under samsung, only laptops (including MacBooks) under laptop, only watches under watch, same for sound and tablet.
+      13. For all model names, ensure that you consistently use the same name even if they look different from the provided data.
+      14. For storage size, always specify the storage unit. e.g. 256GB, 1TB, 128GB.
+      15. For laptop and tablets, add all the product specifications available for a product to Lock Status.
+      16. For MacBooks, always ensure that the model year is included under the model name. e.g. Used MacBook Pro 2020.
+      17. For tablets and laptops, always ensure that all the available specifications are highlighted under lock status.
+      18. Ensure that condition (BRAND NEW or USED) is ALWAYS added to the product name e.g. BRAND NEW iPhone 15 pro max, USED iPhone Xr.
       
-      		    PLEASE NOTE THAT DATA MIGHT COME IN THIS FORMAT TOO:
-		    (   USED SAMSUNG PHONE
-
-			A03S 32GB 90K
-			
-			A12
-			SINGLE 32GB 110K
-			DUAL 32GB 115K
-			DUAL 128GB 130K
-			
-			A13 
-			DUAL 32GB 120K 
-			DUAL 64GB 130K
-   		    )	
-	    	    This is how to how to extract the data. These are the products in this sample; USED Samsung A03S, 32GB storage, and price is 90,000. USED Samsung A12, SINGLE SIM, 32GB Storage, and price is 110,000. 
-	  	    USED Samsung A12, DOUBLE SIM, 32GB Storage, and price is 115,000. USED Samsung A12, DUAL SIM, 128GB Storage, and price is 130,000. USED Samsung A13, DUAL SIM, 32GB Storage, and price is 120,000. 
-		    USED Samsung A13, DUAL SIM, 64GB Storage, and price is 130,000.       		    
-      		    ENSURE THAT CONDITION (BRAND NEW or USED) IS ALWAYS ADDED TO EVERY EXTRACTED PRODUCT NAME. e.g. BRAND NEW iPhone 15 pro max, USED iPhone Xr, USED Samsung A23. If a condition is not specified, specify that the product is "USED" e.g. (USED Macbook Pro 2023)
-		    device_type can either be iphone, samsung, laptop, watch, sound, tablet (all in lower case).
-        	    Please ensure you return a perfect array of objects. I want to be able to parse it with the javascript JSON.parse() function. So, if the file has incomplete data that can lead to an incomplete object, omit it please and ensure only perfect array of objects is returned. This is extremely important.
-	     `;
+      PLEASE NOTE THAT DATA MIGHT COME IN THIS FORMAT TOO:
+      (   USED SAMSUNG PHONE
+        A03S 32GB 90K
+        A12
+        SINGLE 32GB 110K
+        DUAL 32GB 115K
+        DUAL 128GB 130K
+        A13 
+        DUAL 32GB 120K 
+        DUAL 64GB 130K
+      )	
+      This is how to extract the data. These are the products in this sample; USED Samsung A03S, 32GB storage, and price is 90,000. USED Samsung A12, SINGLE SIM, 32GB Storage, and price is 110,000. 
+      USED Samsung A12, DOUBLE SIM, 32GB Storage, and price is 115,000. USED Samsung A12, DUAL SIM, 128GB Storage, and price is 130,000. USED Samsung A13, DUAL SIM, 32GB Storage, and price is 120,000. USED Samsung A13, DUAL SIM, 64GB Storage, and price is 130,000.       		    
+      ENSURE THAT CONDITION (BRAND NEW or USED) IS ALWAYS ADDED TO EVERY EXTRACTED PRODUCT NAME. e.g. BRAND NEW iPhone 15 pro max, USED iPhone Xr, USED Samsung A23. If a condition is not specified, specify that the product is "USED" e.g. (USED MacBook Pro 2023)
+      device_type can either be iphone, samsung, laptop, watch, sound, tablet (all in lower case).
+      Please ensure you return a perfect array of objects. I want to be able to parse it with the javascript JSON.parse() function. So, if the file has incomplete data that can lead to an incomplete object, omit it please and ensure only a perfect array of objects is returned. This is extremely important.
+    `;
 
     console.log('Chunking request');
     try {
@@ -119,7 +124,6 @@ event.on('process', async (data, filePath, title) => {
         model: 'gpt-4o',
       });
       console.log({ response: response.choices[0].message.content });
-      // fs.writeFileSync('gpt-result.txt', response.choices[0].message.content);
       try {
         const temp = JSON.parse(response.choices[0].message.content);
         finalReponseArray = finalReponseArray.concat(temp);
@@ -141,17 +145,16 @@ event.on('process', async (data, filePath, title) => {
           console.log('Deleted File');
         }
       });
-      // Send 'failed' email using Nodemailer
       try {
         const mailOptions = {
           from: process.env.MAILER_FROM_OPTION,
           to: 'horleytech@gmail.com',
           subject: 'File Processing Failed',
           html: `
-				<h1>Failed File Processed</h1>
-				<p>Sorry, your file could not be processed at the moment. 😥</p>
-				<p>Please try again later.</p>
-		  `,
+            <h1>Failed File Processed</h1>
+            <p>Sorry, your file could not be processed at the moment. 😥</p>
+            <p>Please try again later.</p>
+          `,
         };
         const emailResponse = await transporter.sendMail(mailOptions);
         console.log({ emailResponse });
@@ -162,11 +165,9 @@ event.on('process', async (data, filePath, title) => {
       return;
     }
   }
-  // Send group title to Firebase
-  const groupPayload = {
-    name: title,
-  };
 
+  // Send group title to Firebase
+  const groupPayload = { name: title };
   db.collection('groups')
     .add(groupPayload)
     .then((docRef) => {
@@ -178,9 +179,9 @@ event.on('process', async (data, filePath, title) => {
 
   const finalResult = groupAndSortPhones(finalReponseArray);
 
-  // Send prices data to Firebase: Batch add
+  // Batch add prices data to Firebase
   const batch = db.batch();
-  finalResult.forEach((priceDatum, index) => {
+  finalResult.forEach((priceDatum) => {
     const dataToBeAdded = { ...priceDatum, group: title };
     const docRef = db.collection('prices').doc(generateRandomIds());
     console.log({ dataToBeAdded });
@@ -204,18 +205,17 @@ event.on('process', async (data, filePath, title) => {
     }
   });
 
-  // Send 'success' email using Nodemailer
   try {
     const mailOptions = {
       from: process.env.MAILER_FROM_OPTION,
       to: 'horleytech@gmail.com',
       subject: 'File Processing Successful',
       html: `
-		<h1>File Processed</h1>
-		<p>We are excited to inform you that your txt file has been processed.</p>
-		<p>Kindly check our website to visualize the data.</p>
-		<p>Cheers 🥂</p>
-		`,
+        <h1>File Processed</h1>
+        <p>We are excited to inform you that your txt file has been processed.</p>
+        <p>Kindly check our website to visualize the data.</p>
+        <p>Cheers 🥂</p>
+      `,
       text: 'File Processed. Please check the website for details.',
     };
     const emailResponse = await transporter.sendMail(mailOptions);
